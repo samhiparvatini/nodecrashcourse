fecbe1bfe65a7c40d61abe35926b9a7d3c9c6d95
Samhita Parvatini <samhiparvatini@gmail.com>
2024-03-05 11:22:00 -0500

    lesson2

---
+++ global.js
@@ -0,0 +1,10 @@
+// global object
+
+console.log(global);
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('in the interval
